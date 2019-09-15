e757f3fb9fdd2870ddeee7a1698b91958677c26b
William Vieira <william.joao@yahoo.com.br>
2019-09-15 13:23:29 -0300

    Recriação interface FACEBOOK.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './componets/header/Header';
+import PostList from './componets/postList/PostList';
+
+function App() {
+
+    return (
+        <>
+            <Header />
+            <PostList />
+        </>
+    )
+}
+
+export default App
